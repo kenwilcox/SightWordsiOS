9ca57daf72a56ee0b079fb5b2ead33aefb198f6d
Ken Wilcox <wilcoxk@gmail.com>
2015-09-09 07:34:01 -0600

    Initial Commit

---
+++ SightWords/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SightWords
+//
+//  Created by Kenneth Wilcox on 9/9/15.
+//  Copyright © 2015 Kenneth Wilcox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
